80d485e7ad398e68898e5006ff0f9c29337b308a
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-02-26 14:41:37 -0500

    forms

---
+++ forms-td-start-2/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+@ViewChild('f') signupForm: NgForm; //other way
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: ngForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
